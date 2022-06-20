2c6daf0ca3841734c48e2440baa1467027937836
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-06-20 20:39:47 +0200

    ask user if he want to play again

--- highLowNumbers/Program.cs
+++ highLowNumbers/Program.cs
@@ -8,48 +8,53 @@ namespace highLowNumbers
         {
             Console.WriteLine("You have to choose a number between 1 - 100");
             Console.WriteLine("Let's see if you can guess my number. Good Luck");
+            Console.WriteLine("PS : You onlye have 6 attempts");
+            Console.WriteLine("------------------------------------------");
 
             Random random = new Random();
             int minNumber = 0;
-            int maxNumber = 101;
+            int maxNumber = 4;
             int limitGuesses = 5;
             int numberOfGuesses = 0;
             int randomNumber = random.Next(minNumber, maxNumber);
 
             for (int i = 0; i <= limitGuesses; i++)
             {
-               
+
                 Console.WriteLine("Please enter the number you are thinking :");
                 int userNumber = Convert.ToInt32(Console.ReadLine());
-
-
-                    if (randomNumber > userNumber)
-                    {
-                        Console.WriteLine("The number is to low");
-                        Console.WriteLine($"You have only {limitGuesses - numberOfGuesses} left !");
-                        numberOfGuesses++;
-                    }
+                
+                if (randomNumber > userNumber)
+                {
+                    Console.WriteLine("The number is to low");
+                    Console.WriteLine($"You have only {limitGuesses - numberOfGuesses} left !");
+                    numberOfGuesses++;
+                }
                 if (randomNumber < userNumber)
                 {
                     Console.WriteLine("The number is to high");
                     Console.WriteLine($"You have only {limitGuesses - numberOfGuesses} left !");
                     numberOfGuesses++;
-                    if (randomNumber == userNumber)
+                }
+                if (randomNumber == userNumber)
+                {
+                    Console.WriteLine($"YOU WON !!! You tried : {numberOfGuesses} times");                   
+                }
+                if (limitGuesses == numberOfGuesses - 1)
+                {
+                    Console.WriteLine($"You Lost !!! You tried : {numberOfGuesses} times");
+                    Console.WriteLine("Would you like to play again? Press y or n ");
+                    string quit = Convert.ToString(Console.ReadLine());
+                    if (quit == "y")
                     {
-                        Console.WriteLine($"YOU WON !!! You tried : {numberOfGuesses} times");
+                        continue;
                     }
-                    if (limitGuesses == 5)
+                    else
                     {
-                        Console.WriteLine($"You Lost !!! You tried : {numberOfGuesses} times");
-                        Console.WriteLine("Would you like to play again? Press y or n ");
-                        string quit = Convert.ToString(Console.ReadLine());
-                        if (quit == "n")
-                        {
-                            break;
-                        }
+                        break;
                     }
                 }
-            }          
+            }
         }
     }
 }
