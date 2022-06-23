7fddc53fa38efaf4c94723cdc932fced38d3f90b
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-06-23 22:21:53 +0200

    changing the limitGuesses  and for loop

--- highLowNumbers/Program.cs
+++ highLowNumbers/Program.cs
@@ -6,24 +6,27 @@
         {
             Console.WriteLine("You have to choose a number between 1 - 100");
             Console.WriteLine("Let's see if you can guess my number. Good Luck! ");
-            Console.WriteLine("PS : You only have 5 attempts");
+            Console.WriteLine("PS : You only have 6 attempts");
             Console.WriteLine("----------------------------------------------------");
 
             Random random = new Random();
             int minNumber = 0;
             int maxNumber = 4;
-            int limitGuesses = 5;
             int numberOfGuesses = 0;
             int randomNumber = random.Next(minNumber, maxNumber);
-            int number;
+            
             bool playAgain = true;
+
             while (playAgain)
             {
-                for (int i = 0; i < limitGuesses; i--)
+                int number;
+                int limitGuesses = 0;
+                
+
+                for (int i = 5; i >= limitGuesses; i--)
                 {
                     Console.WriteLine("Please enter the number you are thinking :");
                     bool userNumber = int.TryParse(Console.ReadLine(), out number);
-                    
 
                     if (!userNumber)
                     {
@@ -31,35 +34,32 @@
                         playAgain = true;
                     }
                     if (randomNumber > number)
-                    {
+                    { 
                         Console.WriteLine("The number is to low");
-                        Console.WriteLine($"You have only {limitGuesses} left !");
-                        limitGuesses--;
-                        numberOfGuesses++;
+                        Console.WriteLine($"You have only {i} left !");
+                        
                     }
                     if (randomNumber < number)
                     {
                         Console.WriteLine("The number is to high");
-                        Console.WriteLine($"You have only {limitGuesses} left !");
-                        limitGuesses--;
-                        numberOfGuesses++;
+                        Console.WriteLine($"You have only {i} left !");
+                        
                     }
                     if (randomNumber == number)
                     {
-                        Console.WriteLine($"YOU WON !!! You tried : {numberOfGuesses} times");
-                        numberOfGuesses++;
-                        limitGuesses = 5;
-                        break;
+                        Console.WriteLine($"YOU WON !!! You tried : {i} times");
+                        
+
                     }
-                    if (randomNumber != number && limitGuesses == 0)
+                    if (i == 0)
 
                        
                     {
                         Console.WriteLine($"You Lost !!! You tried : {numberOfGuesses} times");
                         Console.WriteLine("Would you like to play again? Press y or n ");
-                        limitGuesses = 5;
+                        i = 0;
                         string quit = Console.ReadLine();
-                        quit = quit.ToUpper();
+                        quit = quit.ToLower();
                         if (quit == "y")
                         {
                             playAgain = true;
